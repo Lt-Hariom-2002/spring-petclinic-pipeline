d699fcfedf3dfeed517e2cabbe0a2e5eb5ba830b
Lt-Hariom-2002 <143435675+Lt-Hariom-2002@users.noreply.github.com>
2025-09-14 14:03:20 +0530

    Update main.tf
    Signed-off-by: Lt-Hariom-2002 <143435675+Lt-Hariom-2002@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,15 +1,52 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "tomcat_server" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t3.micro"
-  security_groups = ["launch-wizard-1"]  # Reference the existing security group
-  key_name      = "hariom"
+
+# Artifact Server
+resource "aws_instance" "artifact_server" {
+  ami             = "ami-0360c520857e3138f"   # Ubuntu AMI
+  instance_type   = "t3.micro"
+  security_groups = ["launch-wizard-1"]       # Use your existing SG
+  key_name        = "hariom"                  # Your key pair name
+
+  tags = {
+    Name = "artifact-server"
+  }
 }
 
+# Application Server
+resource "aws_instance" "app_server" {
+  ami             = "ami-0360c520857e3138f"
+  instance_type   = "t3.micro"
+  security_groups = ["launch-wizard-1"]
+  key_name        = "hariom"
 
+  tags = {
+    Name = "app-server"
+  }
+}
 
-output  "tomcat_server_ip" {
+# Tomcat Server
+resource "aws_instance" "tomcat_server" {
+  ami             = "ami-0360c520857e3138f"
+  instance_type   = "t3.micro"
+  security_groups = ["launch-wizard-1"]
+  key_name        = "hariom"
+
+  tags = {
+    Name = "tomcat-server"
+  }
+}
+
+# Outputs
+output "artifact_server_ip" {
+  value = aws_instance.artifact_server.public_ip
+}
+
+output "app_server_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
 }
